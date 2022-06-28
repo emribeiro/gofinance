0a9bfd2fa546df6ae4dd4e8bd02cc45bf561c79a
Eric Miranda Ribeiro <ktoxlonian@gmail.com>
2022-06-28 15:54:02 -0300

    Initial Commit.

---
+++ src/Components/Welcome.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Text, View } from "react-native";
+
+
+export function Welcome(){
+    return (
+        <View>
+            <Text>Bem Vindo ao App React-Native</Text>
+        </View>
+    )
+}
