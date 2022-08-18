e89a80aee03dcc3e941c64f90735ffa5f40da8d5
Eric Miranda Ribeiro <ktoxlonian@gmail.com>
2022-08-18 16:10:52 -0300

    Styling bottom tabbar

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -3,24 +3,63 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import { Home } from '../pages/Home';
 import { Registro } from '../pages/Registro';
+import { useTheme } from 'styled-components';
+import Feather from '@expo/vector-icons/Feather';
 
 const {Navigator, Screen} = createBottomTabNavigator();
 
 export function AppRoutes(){
-
+    const theme = useTheme();
     return (
-        <Navigator>
+        <Navigator
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: theme.colors.secondary,
+                tabBarInactiveTintColor: theme.colors.text,
+                tabBarLabelPosition: 'beside-icon',
+                tabBarLabelStyle: {
+                    fontFamily: theme.fonts.regular
+                }
+            }}
+        >
             <Screen 
                 name="Home"
                 component={Home}
+                options={{
+                    tabBarIcon: (({color, size}) => 
+                        <Feather 
+                            name='home'
+                            size={size}
+                            color={color}
+                        />
+                    )
+                }}
             />
             <Screen 
                 name="Cadastrar"
                 component={Registro}
+                options={{
+                    tabBarIcon: (({color, size}) => 
+                        <Feather 
+                            name='dollar-sign'
+                            size={size}
+                            color={color}
+                        />
+                    )
+                }}
             />
             <Screen 
                 name="Resumo"
                 component={Registro}
+                options={{
+                    tabBarIcon: (({color, size}) => 
+                        <Feather 
+                            name='pie-chart'
+                            size={size}
+                            color={color}
+                        />
+                    )
+                }}
             />
 
         </Navigator>
